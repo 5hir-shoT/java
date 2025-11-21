d339b9a7331c5ed6c564df076d43daa689b45e22
5hir-shoT <shirshotarafdar@gmail.com>
2025-11-21 15:49:29 +0530

    String declarations

---
+++ practice/variables2.java
@@ -0,0 +1,19 @@
+public class variables2
+{
+    public static void main(String[]args)
+    {
+        String firstName="Alex";
+        String city="New York";
+        String hobby="Playing guitar";
+        String greeting= "Hello, nice to meet you!";
+        String favouriteQuote="Life is like a box of chocolates";
+        String mood="Happy";
+
+        System.out.println("Name: "+ firstName);
+        System.out.println("City: "+ city);
+        System.out.println("Hobby: "+ hobby);
+        System.out.println("Greeting: "+ greeting);
+        System.out.println("Favourite Quote: "+ favouriteQuote);
+        System.out.println("Current Mood: "+ mood);
+    }
+}
